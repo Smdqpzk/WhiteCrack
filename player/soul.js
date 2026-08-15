95693281716a2cc57ffb0c20ce30e520db457535
Smdqpzk <dojcseric@gmail.com>
2026-08-15 20:39:44 +0800

    Update soul.js

--- player/soul.js
+++ player/soul.js
@@ -1,18 +1,23 @@
 export function toggleSoul(player) {
 
-    if (
-        player.soul ===
-        "yang"
-    ) {
+    if (player.soul === "yang") {
 
-        player.soul =
-            "yin";
+        player.soul = "yin";
 
     } else {
 
-        player.soul =
-            "yang";
+        player.soul = "yang";
 
     }
+
+    // 切換時重置動畫
+    player.animationFrame = 0;
+    player.animationTimer = 0;
+    player.moving = false;
+
+    console.log(
+        "Soul switched to:",
+        player.soul
+    );
 
 }
